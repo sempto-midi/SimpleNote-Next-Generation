a33667db686170803dfa8467afc5cb9151b12199
Denzel <denzelcrocker686@gmail.com>
2025-04-28 10:51:57 +0300

    Добавьте файлы проекта.

---
+++ SimpleNoteNG/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SimpleNoteNG
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
